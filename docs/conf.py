ec09dc66bbadc694617b88a1f5c0b13bb01fc774
Electromag-IPCMS <electromag.ipcms@gmail.com>
2025-04-10 14:13:59 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,26 @@
+project = 'Vision2P'
+author = 'CNRS, CroesBoris, Cherifi-Hertel Salia'
+release = '0.1.0'
 
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+]
+
+
+master_doc = 'index' 
+exclude_patterns = []
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
+
+htmlhelp_basename = 'Vision2Pdoc'
+
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '10pt',
+}
