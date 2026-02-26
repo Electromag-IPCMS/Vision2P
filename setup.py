6e3b947bacea0368d8e27af36f9c0b985ac25261
Boris Croes <119600399+BorisCroes@users.noreply.github.com>
2026-02-26 10:19:39 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         ]
     },
     ext_modules=cythonize(extensions, language_level="3"),
-    version='0.1.3',
+    version='0.1.4',
     description='Spectro-microscopy and polarimetry machine learning library',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
