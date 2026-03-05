9d493efb2e391370cc355c56b10e5aaf8fc86e24
Boris Croes <119600399+BorisCroes@users.noreply.github.com>
2026-03-05 14:15:06 +0100

    fix

--- Vision2P/__init__.py
+++ Vision2P/__init__.py
@@ -8,14 +8,15 @@ Authors: Boris Croes and Salia Cherifi-Hertel
 This program is free software: you can redistribute it and/or modify it under the terms of the 3-Clause BSD License. You should have received a copy of the 3-Clause BSD License along with this program.  If not, see <https://opensource.org/license/BSD-3-Clause>.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 Contacts: 
-Boris Croes, IPCMS Strasbourg, 23 rue du Loess,67034 Strasbourg, France. boris.croes@ipcms.unistra.fr
-Salia Cherifi-Hertel, IPCMS Strasbourg, 23 rue du Loess,67034 Strasbourg, France. salia.cherifi@ipcms.unistra.fr
+Boris Croes, IPCMS Strasbourg, 23 rue du Loess, 67034 Strasbourg, France. boris.croes@ipcms.unistra.fr
+Salia Cherifi-Hertel, IPCMS Strasbourg, 23 rue du Loess, 67034 Strasbourg, France. salia.cherifi@ipcms.unistra.fr
 """
 
 from .kmeans import kmeans
 from .nmf import nmf
-from .resultdisplay import ResultDisplay, DisplayConfig, DecompositionResult
 from .processing import Dataset, DataProcessor
+from .resultdisplay import ResultDisplay, DisplayConfig, DecompositionResult
+
 
 __all__ = [
     'Dataset', 
